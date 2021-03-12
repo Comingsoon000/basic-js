dd43fea8c7cc4c82dde0edf5651ab2a42df10610
Comingsoon <comingsoon000@ya.ru>
2021-03-12 14:08:01 +0300

    feat: implement transform(arr) function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('arr is not an Array');
+  }
+  const copy = [...arr];
+  copy.forEach((item, index) => {
+    if (item === '--discard-next') {
+      copy[index] = 'foo';
+      copy[index + 1] = 'foo';
+    }
+    if (item === '--discard-prev') {
+      if (index > 0) {
+        copy[index - 1] = 'foo';
+        copy[index] = 'foo';
+      } else {
+        copy.splice(index, 1);
+      }
+    }
+    if (item === '--double-next') {
+      if (index < copy.length - 1) {
+        copy.splice(index, 1, copy[index + 1]);
+      } else {
+        copy.splice(index, 1);
+      }
+    }
+    if (item === '--double-prev') {
+      if (index > 0) {
+        copy.splice(index, 1, copy[index - 1]);
+      } else {
+        copy.splice(index, 1);
+      }
+    }
+  });
+  return copy.filter((item) => item !== 'foo');
 };
