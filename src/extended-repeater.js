65b039242c5f2d03c4c0893eefd29f73c7514b23
Comingsoon <comingsoon000@ya.ru>
2021-03-13 17:25:13 +0300

    feat: implement repeater(str, options) function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  const stringsArray = [...Array(options.repeatTimes).keys()].map((item) => item = str);
+  const additionsArray = [...Array(options.additionRepeatTimes).keys()].map((item) => item = `${options.addition}`);
+  const addition = additionsArray.join(options.additionSeparator);
+  const result = stringsArray.map((item) => `${item}${addition}`);
+  return result.join(options.separator);
 };
-  
